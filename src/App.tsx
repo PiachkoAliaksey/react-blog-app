d9a665f012b57d6cd7d02970f3de44c3e0970ed6
PiachkoAliaksey <alek17y@mail.ru>
2023-04-02 12:44:39 +0300

    add: basic configuration for  application

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { BrowserRouter, Route, Routes, redirect, Navigate, Link } from 'react-router-dom';
+
+
+
+import './sass/style.scss';
+
+
+function App() {
+  return (
+    <>
+      <h2>Hello World!</h2>
+    </>
+  )
+}
+
+export default App;
